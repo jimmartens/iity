952ce8ba29dc6a3a36116c1d1b1a56013c570859
Jim Martens <jimmartens@gmail.com>
2025-01-23 17:15:29 -0700

    formatting on detail page.

--- frontend/src/views/GuessDetail.vue
+++ frontend/src/views/GuessDetail.vue
@@ -8,12 +8,28 @@
       <div v-else-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
         Error: {{ error }}
       </div>
-
       <div v-else class="space-y-4">
-        <p><strong>Name:</strong> {{ guess.name }}</p>
-        <p><strong>Acronym:</strong> {{ guess.acronym }}</p>
-        <p><strong>Content:</strong> {{ guess.content }}</p>
-        <p><strong>Created At:</strong> {{ guess.created_at }}</p>
+        <table style="width: 100%;">
+          <tr>
+            <td>
+              <p><strong>Name:</strong> {{ guess.name }}</p>
+            </td><td>
+              <p><strong>Created At:</strong> {{ guess.created_at }}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p><strong>Acronym:</strong> {{ guess.acronym }}</p>
+            </td><td>
+              <p><strong>Means:</strong> {{ guess.meaning }} </p> 
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <p><strong>Guess:</strong> {{ guess.content }}</p>
+            </td>
+          </tr>  
+        </table>
       </div>
     </div>
   </div>
@@ -21,6 +37,31 @@
 
 <script>
 import axios from 'axios';
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString)
+    return date.toISOString().slice(0, 10)
+    //return date.toISOString().slice(0, 16).replace('T', ' ')
+}
+const giveMeaning = (acronym) => {
+  // switch statement based on acronym
+  // Could use a map or something similar
+  var meaning = "";
+  switch(acronym) {
+  case "YCHJCYADFTCSO":
+    meaning = "Your curiosity has just cost you a donation for the Colorado Special Olympics.";
+    break;
+  case "YCHJCYAQFTLHPB":
+    meaning = "You curiosity has just coust you a quarter for the Laradon Hall Piggy Bank.";
+    break;
+  case "IITYWYBMAD":
+    meaning = "If I tell you will you buy me a drink?";
+    break;
+  default:
+    meaning = "Acronym not found";
+  }
+  return meaning;
+}
 
 export default {
   name: 'GuessDetail',
@@ -42,6 +83,8 @@ methods: {
         `${this.API_URL}/guesses/${this.$route.params.id}`,
       );
       this.guess = response.data;
+      this.guess.created_at = formatDate(this.guess.created_at);
+      this.guess.meaning = giveMeaning(this.guess.acronym);
     } catch (error) {
       console.error('Error fetching guess:', error);
       this.error = 'Failed to load guess details.';
